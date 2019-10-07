803f4f2da4ffe627e292db527383675d3ec9b6c9
Sharang Pai <Guzzler@users.noreply.github.com>
2019-10-08 02:50:46 +0530

    Feature/animated sprites (#3)
    * added prey animation for walking and movement
    
    * removed some print statements and framerate change

--- prey.gd
+++ prey.gd
@@ -6,20 +6,37 @@ var velocity = Vector2()
 var target = position
 var isClickingObject = false
 var localTarget = null
+var walking = true
+var running = false
+var dead = false
+onready var preySprite = get_node("PreySprite")
+
 func _input(event):
    # Mouse in viewport coordinates
 	if event is InputEventMouseButton:
 		if event.pressed:
 			isClickingObject = false
 			localTarget = get_global_mouse_position()
-			print((localTarget-position).length())
 			if (localTarget-position).length() < 50: 
 				isClickingObject = true
 		if not event.pressed && isClickingObject:
 			target = get_global_mouse_position()
+			
 
 func _physics_process(delta):
 	velocity = (target - position).normalized() * speed
 	if (target - position).length() > 5:
-        velocity = move_and_slide(velocity)
+		velocity = move_and_slide(velocity)
+		walking = false
+		running = true
+	else:
+		walking = true
+		running = false
+		
+	if walking:
+		preySprite.play("walk")
+	if running:
+		preySprite.play("run")
+	if dead: 
+		preySprite.play("dead")
 		
