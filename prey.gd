c5b49e889cce481e03410d5904e535999dab2dd9
Guzzler <sharangpai123@gmail.com>
2019-10-07 19:18:52 +0530

    made basic background scene

---
+++ prey.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+var target = position
+var isClickingObject = false
+var localTarget = null
+func _input(event):
+   # Mouse in viewport coordinates
+	if event is InputEventMouseButton:
+		if event.pressed:
+			isClickingObject = false
+			localTarget = get_global_mouse_position()
+			print((localTarget-position).length())
+			if (localTarget-position).length() < 50: 
+				isClickingObject = true
+		if not event.pressed && isClickingObject:
+			target = get_global_mouse_position()
+
+		print("Mouse Click/Unclick at: ", event.position)
+
+func _physics_process(delta):
+	velocity = (target - position).normalized() * speed
+	if (target - position).length() > 5:
+        velocity = move_and_slide(velocity)
+		
+		
